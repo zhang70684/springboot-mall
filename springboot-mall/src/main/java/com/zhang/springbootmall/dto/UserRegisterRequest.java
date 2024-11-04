84a33dfa37bf8b43912d4e571ee77768ecce4bd6
zhang70684 <zhang70684@gmail.com>
2024-11-04 14:06:37 +0800

    account function - make sure email is email format and hide password

--- springboot-mall/src/main/java/com/zhang/springbootmall/dto/UserRegisterRequest.java
+++ springboot-mall/src/main/java/com/zhang/springbootmall/dto/UserRegisterRequest.java
@@ -1,11 +1,14 @@
 package com.zhang.springbootmall.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class UserRegisterRequest {
 
     @NotBlank
+    @Email
     private String email;
+
     @NotBlank
     private String password;
 
