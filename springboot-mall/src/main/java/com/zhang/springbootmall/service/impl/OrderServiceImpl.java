8bb4ee653105cc51ecd9a0172124a3f099c7b110
zhang70684 <zhang70684@gmail.com>
2024-11-11 14:50:07 +0800

    Order Function - Check userId exist

--- springboot-mall/src/main/java/com/zhang/springbootmall/service/impl/OrderServiceImpl.java
+++ springboot-mall/src/main/java/com/zhang/springbootmall/service/impl/OrderServiceImpl.java
@@ -2,15 +2,21 @@ package com.zhang.springbootmall.service.impl;
 
 import com.zhang.springbootmall.dao.OrderDao;
 import com.zhang.springbootmall.dao.ProductDao;
+import com.zhang.springbootmall.dao.UserDao;
 import com.zhang.springbootmall.dto.BuyItem;
 import com.zhang.springbootmall.dto.CreateOrderRequest;
 import com.zhang.springbootmall.model.Order;
 import com.zhang.springbootmall.model.OrderItem;
 import com.zhang.springbootmall.model.Product;
+import com.zhang.springbootmall.model.User;
 import com.zhang.springbootmall.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +24,16 @@ import java.util.List;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
+
     @Autowired
     private OrderDao orderDao;
 
     @Autowired
     private ProductDao productDao;
+
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public Order getOrderById(Integer orderId) {
@@ -39,6 +50,13 @@ public class OrderServiceImpl implements OrderService {
     @Transactional
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+
+        //驗證 userId 存不存在
+        User user = userDao.getUserById(userId);
+        if(user == null){
+            log.warn("該 userId {} 不存在",userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
 
         int totalAmount = 0;
         List<OrderItem> orderItemList = new ArrayList<>();
